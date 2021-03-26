792ae82953f587bdbd83d2151a7640c7b80592d7
Zeenat-Hassan <79977169+Zeenat-Hassan@users.noreply.github.com>
2021-03-26 18:37:22 +0500

    Update Update_query.sql

--- Update_query.sql
+++ Update_query.sql
@@ -1,5 +1,10 @@
-#This query adds in 70% of records as "training" 
+--This query adds in 70% of records as "training" 
 Update QA1 
 set Data_Type= "Training"
 order by rand()
 limit 1650 offset 0
+
+--This query finds the records ("with the required substring that can be removed/trimed")
+Select * 
+from QA1 
+where Processed_Data like '%kind regards%'
