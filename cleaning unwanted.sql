c94e5fdb3e30e74581f36d4400a4d5ea0fe20b73
Zeenat-Hassan <79977169+Zeenat-Hassan@users.noreply.github.com>
2021-03-30 11:15:13 +0500

    Update cleaning unwanted.sql

--- cleaning unwanted.sql
+++ cleaning unwanted.sql
@@ -2,3 +2,7 @@
 SELECT Original_text,POSITION("--- Device" IN Original_text),length(Original_text),SUBSTRING(Original_text, POSITION("---- Device" IN Original_text), length(Original_text)) 
 from QA_1 
 where Original_text like "%---% Device%"
+
+---Query to remove unanted string---
+Update QA_1
+set Processed_Data= REPLACE(Original_text,SUBSTRING(Original_text, POSITION("---- Device" IN Original_text), length(Original_text))," " )
