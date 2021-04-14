3f50522815750b604d657c5035f5a5b30ba97f47
Zeenat-Hassan <79977169+Zeenat-Hassan@users.noreply.github.com>
2021-04-14 12:37:48 +0500

    Add files via upload

--- new_df_read.py
+++ new_df_read.py
@@ -1,23 +1,24 @@
 import pandas as pd
-import re as re
+import chardet
+import csv
+import math
+with open('/home/syeda.zeenat/Downloads/5-reportJune2019-Jan2020.csv', 'r') as file:
+    reader = csv.reader(file)
+    df = pd.DataFrame(reader)
+    print(df)
+    # for row in reader:
+    #     df = pd.DataFrame(row)
+    #     print(df)
+NUMBER_OF_SPLITS = 3
+fileOpens = [open(f"out{i}.csv","w") for i in range(NUMBER_OF_SPLITS)]
+fileWriters = [csv.writer(v, lineterminator='\n') for v in fileOpens]
+for i,row in df.iterrows():
+    fileWriters[math.floor((i/df.shape[0])*NUMBER_OF_SPLITS)].writerow(row.tolist())
+for file in fileOpens:
+    file.close()
 
-dataset = pd.read_csv ('1-reportMarch2021-April2021.csv', sep= ',',quotechar='"', encoding='utf8',engine="python",error_bad_lines=False)
-new_df = pd.DataFrame(dataset, columns = ["Text for Training","Language"])
-#df.drop(df[df['Language'] < "English" | df['Language'] < "english" ].index, inplace = True)
-# for x in new_df.index:
-#   if new_df.loc[x, "Language"] == "Spanish" or new_df.loc[x, "Language"] == "German" or  new_df.loc[x, "Language"] == "german" or new_df.loc[x, "Language"] == "french" or new_df.loc[x, "Language"] == "French" :
-#     new_df.drop(x, inplace = True)
 
-# for x in new_df.index:
-#   if new_df.loc[x, "Language"] != "English" and new_df.loc[x, "Language"] != "english" :
-#     new_df.drop(x, inplace = True)
-# new_df.to_csv('test_1.csv', sep= ',',quotechar='"', encoding='utf8')
-def find_nonalpha(text):
-    result = re.findall("[^A-Za-z0-9 ]",text)
-    return result
-new_df ['nonalpha']=new_df ['Text for Training'].apply(lambda x: find_nonalpha(x))
-print("\Extracting only non alphanumeric characters from company_code:")
-sr = pd.Series(new_df ['nonalpha'])
-print(sr)
-sr.to_csv('test_2.csv', sep= ',')
-print(new_df.to_string())
+# with open('out0.csv', 'r') as file:
+#     reader = csv.reader(file)
+#     df = pd.DataFrame(reader)
+#     print(df)
